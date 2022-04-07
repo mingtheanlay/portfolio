f13f4118e9cd44ba9c034ddb425df1c782820f6b
mingtheanlay@gmail.com <mingthean.lay@gmail.com>
2022-04-07 15:03:31 +0700

    first commit

---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+import './index.scss'
+import NavBar from '../Sidebar/index'
+
+/*
+ * Layout component
+ */
+
+const Layout = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default Layout
